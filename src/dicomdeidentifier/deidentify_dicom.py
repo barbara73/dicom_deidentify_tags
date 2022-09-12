b3ee597f446d5fe5e25288c76c91204fceda3281
barbara73 <barbara.jesacher@gmail.com>
2022-09-12 13:55:25 +0200

    corrections on tags

--- src/dicomdeidentifier/deidentify_dicom.py
+++ src/dicomdeidentifier/deidentify_dicom.py
@@ -153,8 +153,7 @@ class DeidentifyDataset:
                 rule_sets=[timeshift_custom_ruleset,
                            no_times_ruleset,  # delete all times
                            ])
-            ds_1 = Dataset()
-            ds_1.CodeValue = ''
+            ds_1 = None
         else:
             profile = Profile(
                 rule_sets=[timeshift_custom_ruleset,
